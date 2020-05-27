5dcc6403ad163407de9f4761c3e22a1ae1e5bdf9
ryanl13214 <rlangridge132@hotmail.com>
2020-05-27 11:48:10 +0100

    Revert "adding project"
    This reverts commit 106783ce079d2085c8582114f265b5b460214eba.

--- src/about.js
+++ src/about.js
@@ -1,6 +1,6 @@
 /** @format */
 import React from 'react';
-import {  Image,  PixelRatio,  StyleSheet,  Text,  TouchableOpacity,  View,   SafeAreaView,ScrollView} from 'react-native';
+import {  Image,  PixelRatio,  StyleSheet,  Text,  TouchableOpacity,  View,   Button} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
 
@@ -53,19 +53,33 @@ export default class  About extends React.Component {
     return (
 
 
-      <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-        </ScrollView>
-      </SafeAreaView>
+
+      <View style={{  alignItems: 'center', justifyContent: 'flex-start' }}>
+<Image style={{width: "100%",  height: "60%" }} source={ this.props.route.params.newIMG}/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+        <Button
+          title="home"
+
+      onPress={() => this.props.navigation.navigate('Home' )}
+        />
+      </View>
 
 
 
