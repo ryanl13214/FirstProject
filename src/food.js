6685aabedc4fa9e285621d5abfe255571c37215e
ryanl13214 <rlangridge132@hotmail.com>
2020-07-02 14:10:23 +0100

    chneged the food section home page
    added in the table and an area for the chart

--- src/food.js
+++ src/food.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {  Image,  PixelRatio,  StyleSheet,  Text,  TouchableOpacity,  View, ScrollView,  Button,SafeAreaView} from 'react-native';
 
 
-
+import { Dimensions } from 'react-native'
 
 
 export default class  Food extends React.Component {
@@ -12,18 +12,20 @@ export default class  Food extends React.Component {
     super(props);
   }
   render() {
+    const screenHeight = Dimensions.get('window').height;
+    const seven = Dimensions.get('window').height *0.30;
     return (
 
+ <View style={{height: screenHeight}}>
 
-      <SafeAreaView style={styles.container}>
-          <View style={{ marginTop: "-20%" ,height: "70%"}}>
+  <ScrollView vertical={true}  >
 
-              <ScrollView vertical={true}>
-/* ===============================================================================================================================================*/
+          <View style={{ flex: 1,marginTop: "20%" ,height: "70%"}}>
+
+              <View  >
                   <View style={{ marginLeft: "10%" }}>
                       <Text style={styles.text}>Daily journies </Text>
                   </View>
-/* ===============================================================================================================================================*/
                   <View style={styles.button} onPress={()=> this.props.navigation.navigate('food')}>
                       <View style={{ width: "100%",height: "100%", flexDirection: 'column'}}>
                           <View style={{ width: "100%",height: "40%" , justifyContent: 'center',alignItems: 'center', flexDirection: 'column', backgroundColor: "rgb(57, 192, 175)" , borderRadius:30,}}>
@@ -37,7 +39,7 @@ export default class  Food extends React.Component {
 
                       </View>
                   </View>
-/* ===============================================================================================================================================*/
+
                   <View style={styles.button} onPress={()=> this.props.navigation.navigate('food')}>
                       <View style={{ width: "100%",height: "100%", flexDirection: 'column'}}>
 
@@ -52,7 +54,7 @@ export default class  Food extends React.Component {
 
                       </View>
                   </View>
-/* ===============================================================================================================================================*/
+
                   <View style={styles.button} onPress={()=> this.props.navigation.navigate('food')}>
                       <View style={{ width: "100%",height: "100%", flexDirection: 'column'}}>
 
@@ -67,12 +69,92 @@ export default class  Food extends React.Component {
 
                       </View>
                   </View>
-/* ===============================================================================================================================================*/
 
-          </ScrollView>
-         </View>
-      </SafeAreaView>
 
+
+              </View>
+          </View>
+
+
+          <View style={{flex: 1, width: "80%",height: seven, flexDirection: 'column',backgroundColor:"green"  ,marginLeft:"10%",marginTop:"20%", borderRadius: 15}}>
+
+              <View style={{ width: "100%",height: "33%"  , flexDirection: 'column' ,  borderColor:  'black', borderWidth:  1, borderTopRightRadius: 15,  borderTopLeftRadius: 15,}}>
+
+<Text>Calories: 1888/300</Text>
+
+              </View>
+
+              <View style={{ width: "100%",height: "23%"  , flexDirection: 'row'  ,  borderColor:  'black', borderWidth:  1, borderRadiusTopLeft:15,  flexDirection: 'row'}}>
+                  <View style={{ width: "50%",height: "100%"   }}>
+
+                  </View>
+
+                  <View style={{ width: "50%",height: "100%"  ,    borderColor:  'black', borderLeftWidth:  1 }}>
+
+                  </View>
+              </View>
+
+              <View style={{ width: "100%",height: "23%"  , flexDirection: 'row' ,  borderColor:  'black', borderWidth:  1, borderRadiusTopLeft:15,}}>
+                  <View style={{ width: "50%",height: "100%"   }}>
+
+                  </View>
+
+                  <View style={{ width: "50%",height: "100%"  ,    borderColor:  'black', borderLeftWidth:  1 }}>
+
+                  </View>
+              </View>
+
+              <View style={{ width: "100%",height: "21%"  , flexDirection: 'row'  ,  borderColor:  'black', borderWidth:  1,  borderBottomLeftRadius: 15, borderBottomRightRadius: 15}}>
+                  <View style={{ width: "50%",height: "100%"   }}>
+
+                  </View>
+
+                  <View style={{ width: "50%",height: "100%"  ,    borderColor:  'black', borderLeftWidth:  1 }}>
+
+                  </View>
+              </View>
+
+          </View>
+
+
+
+
+
+
+          <View style={{flex: 1, width: "80%",height: seven, flexDirection: 'column',backgroundColor:"green"  ,marginLeft:"10%",marginTop:"20%" }}>
+
+
+
+          </View>
+
+
+
+
+
+
+
+        <View style={{flex: 1, width: "80%",height: seven, flexDirection: 'column'}}>
+        </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </ScrollView>
+
+  </View>
 
 
     );
@@ -89,11 +171,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     backgroundColor:"skyblue",
 },
-container: {
-flex: 1,
-alignItems: "flex-start",
-justifyContent: "center"
-},
+
 
 
 button: {
@@ -101,7 +179,7 @@ button: {
   margin:10,
   width:"80%",
   marginLeft:"10%",
-  height:220,
+  height:160,
 
   flexDirection: 'row',
   justifyContent: "center",
--- src/ovulationTracker.js
+++ src/ovulationTracker.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {  Image,  PixelRatio,  StyleSheet,  Text,  TouchableOpacity,  View, ScrollView,  Button,SafeAreaView} from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
 
 
 
@@ -10,63 +11,72 @@ export default class  Food extends React.Component {
 
   constructor(props ) {
     super(props);
+
+    this.items = [
+     'Goa',
+     'Gujrat',
+     'Madhya Pradesh',
+     'Assam',
+     'Gujrat',
+     'Karnataka',
+     'Jharkhand',
+     'Himachal Pradesh',
+     'Manipur',
+     'Meghalaya',
+     'Mizoram',
+     'Uttarakhand',
+     'West Bengal',
+     'Tamil Nadu ',
+     'Punjab',
+     'Rajasthan',
+     'Bihar',
+     'Andhra Pradesh',
+     'Arunachal Pradesh',
+   ];
   }
   render() {
+
+
+
+
     return (
 
       <SafeAreaView style={styles.container}>
 
-      <View style={{  marginTop:"-20%" ,height:"70%"}}>
-
-            <View style={{  marginLeft:"10%" }} >
-                  <Text  style={styles.text}>Daily journies </Text>
-            </View>
 
 
 
 
-              <View style={styles.button}  onPress={() => this.props.navigation.navigate('food')}>
-                  <View style={{ width:"100%",height:"100%",  flexDirection: 'column',  backgroundColor:"red" }}  >
-                  <View style={{ width:"100%",height:"50%",  flexDirection: 'column',  backgroundColor:"green" }}  >
-                  </View>
-                  <Image style={{  width: '10%', height: '10%' ,  flexDirection: 'column'}} source={require('../imgs/sunset.jpg')} />
-                  </View>
-              </View>
-
-
-              <View style={styles.button}  onPress={() => this.props.navigation.navigate('food')}>
-                  <View style={{ width:"100%",height:"100%",  flexDirection: 'column',  backgroundColor:"red" }}  >
-                  <View style={{ width:"100%",height:"50%",  flexDirection: 'column',  backgroundColor:"green" }}  >
-                  </View>
-                  <Image style={{  width: '10%', height: '10%' ,  flexDirection: 'column'}} source={require('../imgs/sunset.jpg')} />
-                  </View>
-              </View>
-
-
-              <View style={styles.button}  onPress={() => this.props.navigation.navigate('food')}>
-                  <View style={{ width:"100%",height:"100%",  flexDirection: 'column',  backgroundColor:"red" }}  >
-                  <View style={{ width:"100%",height:"50%",  flexDirection: 'column',  backgroundColor:"green" }}  >
-                  </View>
-                  <Image style={{  width: '10%', height: '10%' ,  flexDirection: 'column'}} source={require('../imgs/sunset.jpg')} />
-                  </View>
-              </View>
 
 
 
-      </View>
+      <LinearGradient colors={['white', '#bfe6ff', '#59bfff' ]} style={styles.linearGradient}>
+      </LinearGradient>
+
+
+
+
+      {this.items.map((item, key) => (
+        //key is the index of the array
+        //item is the single item of the array
+        <View key={key} style={styles.item}>
+          <Text style={styles.text}>{key}. {item}</Text>
+          <View style={styles.separator} />
+        </View>
+      ))}
 
 
 
       </SafeAreaView>
-
-
-
 
     );
   }
 }
 
 const styles = StyleSheet.create({
+  linearGradient: {
+ flex: 1, position: 'absolute', width: '100%', height: '100%', justifyContent: 'center'
+},
   text: {
     fontSize: 20,
     width:180,
@@ -86,25 +96,23 @@ justifyContent: "center"
 button: {
   flex: 1,
   margin:10,
-  width:"80%",
-  marginLeft:"10%",
+  width:180,
   height:220,
-
-  flexDirection: 'row',
+  backgroundColor:"red",
+   flexDirection: 'row',
   justifyContent: "center",
-  borderTopRightRadius:30,
-  borderTopLeftRadius:30,
-  borderBottomWidth:  10,
-  overflow: 'hidden'
+    borderRadius:30,
+      overflow: 'hidden'
 },
 
 
-buttonwide: {
+buttonWide: {
     flex: 1,
     margin:10,
+    marginLeft:"5%",
       borderRadius:30,
-    width:300,
-    height:200,
+    width:"90%",
+    height:100,
     backgroundColor:"red",
      flexDirection: 'row',
     justifyContent: "center",
