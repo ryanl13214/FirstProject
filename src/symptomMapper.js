c23db5979a9c93b5126429a0e43b9295ce47669f
ryanl13214 <rlangridge132@hotmail.com>
2020-08-14 20:02:20 +0100

    body map partally done

--- src/symptomMapper.js
+++ src/symptomMapper.js
@@ -1,11 +1,12 @@
 /** @format */
 import * as React from 'react';
-import {  Image,  PixelRatio,  StyleSheet,  Text,  TouchableOpacity,  FlatList, View, ScrollView,  Button,SafeAreaView,Scrollview, Alert } from 'react-native';
+import {Modal,  Image,  PixelRatio,  StyleSheet,  Text,  TouchableOpacity,  FlatList, View, ScrollView,  Button,SafeAreaView,Scrollview, Alert } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ImageMapper from 'react-native-image-mapper';
 import { Dimensions } from 'react-native';
+import SyncStorage from 'sync-storage';
 
-
+import { Menu, MenuProvider, MenuOptions, MenuOption, MenuTrigger} from "react-native-popup-menu";
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -13,7 +14,7 @@ const imgheight = height*0.69 ;
 const imgwidth = imgheight*0.47;
 
 // const symptoms=[("Irregular or no ovulation",0),("weight gain",0)];
-const symptoms=[("Irregular or no ovulation",0),("weight gain",0),("acne or oily skin",1),("Thinning hair",0),("Hirsutism",1),("Infertility",0),("Obesity",0),("Low energy",0),("Low self-esteem",0),("Depression",0),("Anxiety",0),("Skin tags",1),("Acanthosis",0),("High Cholesterol",0),( "Type 2 Diabetes",0),("cardiovascular issues",0)];
+const symptoms=[  {  key:"Irregular or no ovulation",map:0},("weight gain",0),("acne or oily skin",1),("Thinning hair",0),("Hirsutism",1),("Infertility",0),("Obesity",0),("Low energy",0),("Low self-esteem",0),("Depression",0),("Anxiety",0),("Skin tags",1),("Acanthosis",0),("High Cholesterol",0),( "Type 2 Diabetes",0),("cardiovascular issues",0)];
 
 
 
@@ -55,7 +56,7 @@ const RECTANGLE_MAP = [
     y2: gety2(100), // y end
     x1: getx2( 35),// x start
     y1: gety2(95), // y start
- prefill: "red",
+
     fill: 'blue',
   },
   {
@@ -66,7 +67,7 @@ const RECTANGLE_MAP = [
     y2: gety2(100), // y end
     x1: getx2( 51),// x start
     y1: gety2(95), // y start
- prefill: "red",
+
     fill: 'blue',
   },
   {
@@ -77,7 +78,7 @@ const RECTANGLE_MAP = [
     y2: gety2(75), // y end
     x1: getx2( 35),// x start
     y1: gety2(70), // y start
- prefill: "red",
+
     fill: 'blue',
   },
   {
@@ -88,7 +89,7 @@ const RECTANGLE_MAP = [
     y2: gety2(75), // y end
     x1: getx2( 51),// x start
     y1: gety2(70), // y start
- prefill: "red",
+
     fill: 'blue',
   },
   {
@@ -99,7 +100,7 @@ const RECTANGLE_MAP = [
     y2: gety2(45), // y end
     x1: getx2( 34),// x start
     y1: gety2(30), // y start
- prefill: "red",
+
     fill: 'blue',
   },
   {
@@ -110,7 +111,7 @@ const RECTANGLE_MAP = [
     y2: gety2(57), // y end
     x1: getx2( 1),// x start
     y1: gety2(48), // y start
- prefill: "red",
+
     fill: 'blue',
   },
   {
@@ -121,7 +122,7 @@ const RECTANGLE_MAP = [
     y2: gety2(57), // y end
     x1: getx2( 88),// x start
     y1: gety2(48), // y start
- prefill: "red",
+
     fill: 'blue',
   },
 
@@ -133,7 +134,7 @@ const RECTANGLE_MAP = [
     y2: gety2(14), // y end
     x1: getx2( 40),// x start
     y1: gety2(0), // y start
- prefill: "red",
+
     fill: 'blue',
   },
 ];
@@ -164,12 +165,12 @@ function Item({ props,title}) {
      <View  style={{marginLeft:"10%", flexDirection:"row", width:"50%" , height:"100%"  }}>
 
        <View  style={{  width:"50%" , height:"100%"  }}>
-       <Image style={{   width:50, height:  50 }} source={require('../imgs/grap.png')} />
-       <Text numberOfLines={ 1 }   adjustsFontSizeToFit style={{textAlign: 'center',width:"100%" ,  marginLeft:"-15%" ,  height:  20 }}  >View History</Text>
+       <Image style={{   width:40, height: 40,marginBottom:10 }} source={require('../imgs/grap.png')} />
+       <Text numberOfLines={ 1 }   adjustsFontSizeToFit style={{ width:"100%" ,  marginLeft:"-15%" ,  height:  20 }}  >View History</Text>
        </View>
        <View  style={{  width:"50%" , height:"100%"  }}>
          <TouchableOpacity style={{ width:50, height:  50}}  onPress={() =>  props.navigation.navigate('tbabs')} >
-            <Image style={{  width:50, height:  50  }} source={require('../imgs/add.png')}  />
+            <Image style={{  width:40, height:  40  }} source={require('../imgs/update.png')}  />
          </TouchableOpacity>
 
          <Text  numberOfLines={ 1 }   adjustsFontSizeToFit style={{ width:"100%" , marginLeft:"2%" ,  height:  20 }}  >update</Text>
@@ -202,11 +203,28 @@ export default class  Symptommapper extends React.Component {
 
   constructor(props ) {
     super(props);
+
+
+
+this.state.flatlistdata=[  {  key:'Irregular or no ovulation' },    {  key:'Acne' },    {  key:'Type 2 Diabetes' }]
+
+
+for (var i=0;i<4;i++){
+this.state.flatlistdata.push( {  key:i.toString() } );
+
+}
+
+
+
+
   }
 
 
 
-
+  state = {
+    isVisible: false, //state of modal default false
+    flatlistdata:[]
+  }
 
 
   render() {
@@ -215,7 +233,7 @@ export default class  Symptommapper extends React.Component {
       return (
 
         <View  style={{ flex: 1,   padding: 30,position:"absolute" ,top:"2%",left:"9%" ,width:"85%" , height:"100%"  }}>
-
+<Text style={[styles.text,{position:"absolute" ,top:"2%",left:"-10%",width:"32%", height:"20%" }]}> select location on the body</Text>
         <ImageMapper
         style={{  }}
         imgHeight={imgheight}
@@ -230,7 +248,7 @@ export default class  Symptommapper extends React.Component {
 
 
 
-
+<Text style={[styles.text,{position:"absolute" ,top:"96%",left:"80%",width:"32%", height:"5%" ,backgroundColor:"white"}]}> Done</Text>
             <View   style={{ height:50 ,width:1 }}></View>
           </View>
 
@@ -245,28 +263,72 @@ export default class  Symptommapper extends React.Component {
 
 
     const Lisr = ({ navigation }) => {
+      var data = [["Big Data", "Hadoop", "Spark", "Hive"], ["Data Science" ,"Python","Ruby"]];
 
       return (
           <SafeAreaView style={styles.container}>
 <FlatList
-      data={[
-            {  key:'user Symptom 1',symptom:"Irregular or no ovulation" },    {  key:'user Symptom 2',symptom:"Acne" },    {  key:'user Symptom 3',symptom:"Type 2 Diabetes" }
-            ]}
+      data={this.state.flatlistdata}
             keyExtractor={item => item.key.toString()}
-      renderItem={({ item }) => <Item title={item.symptom} props={this.props} />}
+      renderItem={({ item }) => <Item title={item.key} props={this.props} />}
       style={{ fontSize: 15      ,width:"95%" ,marginLeft:3 ,marginRight:1 }}
     />
 
 
 
+    <Modal animationType = {"slide"} transparent = {true}
+        visible = {this.state.isVisible}
+        onRequestClose = {() =>{ console.log("Modal has been closed.") } }>
 
+        <View style = {{width:"70%",height:  "70%",backgroundColor:"white" ,marginLeft:"15%",marginTop:"15%",borderRadius:15}}>
+          <View style = {{width:"100%",height:  "90%"}}>
+
+          <MenuProvider style={{ flexDirection: "column", padding: 30 }}>
+              <Menu onSelect={value => alert(`You Clicked : ${value}`)}>
+
+                <MenuTrigger  >
+                <Text style={styles.headerText}>Select Symptom</Text>
+                </MenuTrigger  >
+
+                <MenuOptions>
+                  <MenuOption value={"Symptom1"}>
+                    <Text style={styles.menuContent}>Symptom1</Text>
+                  </MenuOption>
+                  <MenuOption value={"Symptom2"}>
+                    <Text style={styles.menuContent}>Symptom2</Text>
+                  </MenuOption>
+                  <MenuOption value={"Symptom3"}>
+                    <Text style={styles.menuContent}>Symptom3</Text>
+                  </MenuOption>
+                  <MenuOption value={"Symptom4"}>
+                    <Text style={styles.menuContent}>Symptom4</Text>
+                  </MenuOption>
+
+                </MenuOptions>
+
+              </Menu>
+            </MenuProvider>
+
+
+          </View>
+          <View   style={{  width:"100%",flexDirection:"row" }}>
+          <Button  style = {{ width:"100%",height:  "10%",borderRadius:15}} title="Close" onPress = {() => {this.setState({ isVisible:!this.state.isVisible})}}/>
+          <Button  style = {{ width:"100%",height:  "10%",borderRadius:15}} title="Done" onPress = {() => {this.setState({ isVisible:!this.state.isVisible})}}/>
+          </View>
+        </View>
+
+
+
+    </Modal>
 
 
 
   <View   style={{flexDirection:"row", height:80 ,width:"100%" }}>
     <View   style={{ width:width-200 }}></View>
 <Text  style={{  marginTop:30 ,  height:  20 }}  >Add new symptom</Text>
-<Image style={{   width:50, height:  50 }} source={require('../imgs/add.png')} />
+<TouchableOpacity style={{ width:50, height:  50}}  onPress = {() => {this.setState({ isVisible:!this.state.isVisible})}} >
+   <Image style={{  width:50, height:  50  }} source={require('../imgs/add.png')}  />
+</TouchableOpacity>
   </View>
 
 
@@ -327,14 +389,14 @@ export default class  Symptommapper extends React.Component {
               name="FirstPage"
               component={Lisr}
               options={{
-                tabBarLabel: 'Home',
+                tabBarLabel: 'Symptom list',
 
               }}  />
               <Tab.Screen
                 name="tbabs"
                 component={Map}
                 options={{
-                  tabBarLabel: 'r',
+                  tabBarLabel: 'Body map',
 
                 }}  />
 
@@ -353,14 +415,24 @@ const styles = StyleSheet.create({
     marginLeft:9,
     borderRadius:15,
     textAlign: 'center',
-    backgroundColor:"skyblue",
+   
 },
 container: {
 flex: 1,
 alignItems: "flex-start",
 justifyContent: "center"
 },
-
+headerText: {
+fontSize: 20,
+margin: 10,
+fontWeight: "bold"
+},
+menuContent: {
+color: "#000",
+fontWeight: "bold",
+padding: 2,
+fontSize: 20
+},
 
 button: {
   flex: 1,
